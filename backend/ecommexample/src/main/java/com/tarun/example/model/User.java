525a23d9e7eadb56103bf8e9c2729a4be00658d3
tarun7180 <mailtarunjain@gmail.com>
2024-05-19 18:30:26 +0530

    a

---
+++ backend/ecommexample/src/main/java/com/tarun/example/model/User.java
@@ -0,0 +1,80 @@
+package com.tarun.example.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "user")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "user_name")
+	private String userName;
+	
+	@Column(name = "user_email")
+	private String userEmail;
+	
+	@Column(name = "user_password")
+	private String userPassword;
+
+	public User() {
+		super();
+	}
+
+	public User(Long id, String userName, String userEmail, String userPassword) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.userEmail = userEmail;
+		this.userPassword = userPassword;
+	}
+
+	public User(String userName, String userEmail, String userPassword) {
+		super();
+		this.userName = userName;
+		this.userEmail = userEmail;
+		this.userPassword = userPassword;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+}

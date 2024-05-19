e3d78dff0b5d7a742c4ca721a134a69f8cf0e22e
tarun7180 <mailtarunjain@gmail.com>
2024-05-19 17:16:58 +0530

    a

---
+++ backend/ecommexample/src/main/java/com/tarun/example/EcommexampleApplication.java
@@ -0,0 +1,13 @@
+package com.tarun.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommexampleApplication.class, args);
+	}
+
+}

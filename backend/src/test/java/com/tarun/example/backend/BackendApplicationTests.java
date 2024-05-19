efba67bd28f71816448325c95701098b94523476
tarun7180 <mailtarunjain@gmail.com>
2024-05-19 14:08:17 +0530

    first commit

---
+++ backend/src/test/java/com/tarun/example/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarun.example.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
